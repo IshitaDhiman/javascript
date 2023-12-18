3483854f6b62267b57930201c98b745ab444f807
IshitaDhiman <105802725+IshitaDhiman@users.noreply.github.com>
2023-12-18 14:41:46 +0000

    objects practice

--- basics/objects2.js
+++ basics/objects2.js
@@ -21,7 +21,7 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
-const obj3 = { obj1, obj2 }
+const obj3 = { obj1, obj2 }//object inside object
 console.log(obj3);
 
 const obj5 = Object.assign({}, obj1, obj2, obj4)//target,source
